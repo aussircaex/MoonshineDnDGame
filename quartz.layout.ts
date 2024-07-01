e12c1e04a751a059d4a8baefeeb1d3c7bbf9b89f
aussircaex <evan.a.paris@gmail.com>
2024-07-01 00:03:15 -0400

    Quartz sync: Jul 1, 2024, 12:03 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,9 +29,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+	Component.TableOfContents(),
+	Component.MobileOnly(Component.Explorer()),
   ],
 }
 
